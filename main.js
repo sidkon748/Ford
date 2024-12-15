872ba5d2baeecb3e07410c8367b56288caac3ce3
Siddhartha Konduru <kondurus@usf.edu>
2024-12-15 14:13:50 -0500

    Add document.getElementById

---
+++ main.js
@@ -0,0 +1,6 @@
+const addTaskButton = document.getElementById('addTaskButton');
+const newTaskInput = document.getElementById('newTaskInput');
+const taskList = document.getElementById('taskList');
+const allTasksButton = document.getElementById('allTasks');
+const completedTasksButton = document.getElementById('completedTasks');
+const pendingTasksButton = document.getElementById('pendingTasks');
